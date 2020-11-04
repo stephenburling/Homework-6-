cf4766aeecb54a66db1b1871d9e41c54a43110bf
stephenburling <stephenburling99@gmail.com>
2020-11-03 21:41:05 -0600

    testing call.cpp again

--- call.cpp
+++ call.cpp
@@ -3,20 +3,45 @@
 #include "dlist.h"
 
 struct Member {
-    int number_of_ticks;
+    int time_stamp;
     std::string name;
     std::string level;
     int call_duration;
 };
+/*
+void ReadCalls(Dlist<Member>& caller_queue) {
+    Dlist<Member> new_list;
+    while (!caller_queue.IsEmpty()) {
+        Member current_member = caller_queue.RemoveBack();
+        if (current_member.time_stamp != 0) {
+            current_member.time_stamp = current_member.time_stamp - 1;
+            new_list.InsertFront(current_member);
+        }
+        else {
+            if (current_member.level == "none") {
+                std::cout << "Call from " << current_member.name << " a regular member" << std::endl;
+            }
+            else {
+                std::cout << "Call from " << current_member.name << " a " << current_member.level << " member" << std::endl;
+            }
+        }
+        if (caller_queue.IsEmpty()) {
+            caller_queue = new_list;
+            break;
+        }
+    }
+}
+*/
 
-int CallReader(Dlist<Member>& caller_queue, int caller_duration, std::string name, int ticks) {
+int BusyCallReader(Dlist<Member>& caller_queue, int caller_duration, std::string name, int ticks) {
     for (int i = 0; i < caller_duration; i++) {
+        int spot = 0;
+        Dlist<Member> new_list;
         while (!caller_queue.IsEmpty()) {
-            Dlist<Member> new_list;
             Member current_member = caller_queue.RemoveBack();
-            if (current_member.number_of_ticks != 0) {
-                current_member.number_of_ticks = current_member.number_of_ticks - 1;
-                new_list.InsertBack(current_member);
+            if (current_member.time_stamp != ticks) {
+                new_list.InsertFront(current_member);
+                spot++;
             }
             else {
                 if (current_member.level == "none") {
@@ -34,8 +59,11 @@ int CallReader(Dlist<Member>& caller_queue, int caller_duration, std::string nam
         if (i == 0) {
             std::cout << "Answering call from " << name << std::endl;
         }
-        ticks++;
-        std::cout << "Starting tick #" << ticks << std::endl;
+        if ((i + 1) != caller_duration) {
+            ticks++;
+            std::cout << "Starting tick #" << ticks << std::endl;
+        }
+        
     }
     return ticks;
 }
@@ -45,30 +73,111 @@ void AnswerDriver(Dlist<Member>& caller_queue, Dlist<Member>& platinum_queue, Dl
     int ticks = 0;
     std::cout << "Starting tick #" << ticks << std::endl;
     while (!queue_cleared) {
-        if (!platinum_queue.IsEmpty()) {
-            Member current_member = platinum_queue.RemoveBack();
 
-            ticks = CallReader(caller_queue, current_member.call_duration, current_member.name, ticks);
+        queue_cleared = true;
+        bool call_reader_ran = false;
 
-        }else if (!gold_queue.IsEmpty()) {
-            Member current_member = gold_queue.RemoveBack();
-
-            ticks = CallReader(caller_queue, current_member.call_duration, current_member.name, ticks);
-
-        } else if(!silver_queue.IsEmpty()) {
-            Member current_member = silver_queue.RemoveBack();
-
-            ticks = CallReader(caller_queue, current_member.call_duration, current_member.name, ticks);
-
-        }else if (!none_queue.IsEmpty()) {
-            Member current_member = none_queue.RemoveBack();
-
-            ticks = CallReader(caller_queue, current_member.call_duration, current_member.name, ticks);
-
+        if (!platinum_queue.IsEmpty() && (call_reader_ran == false)) {
+            queue_cleared = false;
+            bool search_queue = true;
+            Dlist<Member> temp_list;
+            while (search_queue == true) {
+                Member current_member = platinum_queue.RemoveBack();
+                //std::cout << "   Checking member " << current_member.name << " with time stamp number     " << current_member.time_stamp << "      ANSWER_DRIVER" << std::endl;
+                if (ticks >= current_member.time_stamp) {
+                    ticks = BusyCallReader(caller_queue, current_member.call_duration, current_member.name, ticks);
+                    call_reader_ran = true;
+                    search_queue = false;
+                }
+                else {
+                    temp_list.InsertBack(current_member);
+                }
+                if (platinum_queue.IsEmpty()) {
+                    while (!temp_list.IsEmpty()) {
+                        platinum_queue.InsertFront(temp_list.RemoveBack());
+                    }
+                    search_queue = false;
+                }
+            }
         }
-        else {
-            queue_cleared = true;
+
+        if (!gold_queue.IsEmpty() && (call_reader_ran == false)) {
+            queue_cleared = false;
+            bool search_queue = true;
+            Dlist<Member> temp_list;
+            while (search_queue == true) {
+                Member current_member = gold_queue.RemoveBack();
+                //std::cout << "   Checking member " << current_member.name << " with time stamp number     " << current_member.time_stamp << "      ANSWER_DRIVER" << std::endl;
+                if (ticks >= current_member.time_stamp) {
+                    ticks = BusyCallReader(caller_queue, current_member.call_duration, current_member.name, ticks);
+                    call_reader_ran = true;
+                    search_queue = false;
+                }
+                else {
+                    temp_list.InsertBack(current_member);
+                }
+                if (gold_queue.IsEmpty()) {
+                    while (!temp_list.IsEmpty()) {
+                        gold_queue.InsertFront(temp_list.RemoveBack());
+                    }
+                    search_queue = false;
+                }
+            }
         }
+
+        if(!silver_queue.IsEmpty() && (call_reader_ran == false)) {
+            queue_cleared = false;
+            bool search_queue = true;
+            Dlist<Member> temp_list;
+            while (search_queue == true) {
+                Member current_member = silver_queue.RemoveBack();
+                //std::cout << "   Checking member " << current_member.name << " with time stamp number     " << current_member.time_stamp << "      ANSWER_DRIVER" << std::endl;
+                if (ticks >= current_member.time_stamp) {
+                    ticks = BusyCallReader(caller_queue, current_member.call_duration, current_member.name, ticks);
+                    call_reader_ran = true;
+                    search_queue = false;
+                }
+                else {
+                    temp_list.InsertBack(current_member);
+                }
+                if (silver_queue.IsEmpty()) {
+                    while (!temp_list.IsEmpty()) {
+                        silver_queue.InsertFront(temp_list.RemoveBack());
+                    }
+                    search_queue = false;
+                }
+            }
+        }
+
+        if (!none_queue.IsEmpty() && (call_reader_ran == false)) {
+            queue_cleared = false;
+            bool search_queue = true;
+            Dlist<Member> temp_list;
+            while (search_queue == true) {
+                Member current_member = none_queue.RemoveBack();
+                //std::cout << "   Checking member " << current_member.name << " with time stamp number     " << current_member.time_stamp << "      ANSWER_DRIVER" << std::endl;
+                if (ticks >= current_member.time_stamp) {
+                    ticks = BusyCallReader(caller_queue, current_member.call_duration, current_member.name, ticks);
+                    call_reader_ran = true;
+                    search_queue = false;
+                }
+                else {
+                    temp_list.InsertBack(current_member);
+                }
+                if (none_queue.IsEmpty()) {
+                    while (!temp_list.IsEmpty()) {
+                        none_queue.InsertFront(temp_list.RemoveBack());
+                    }
+                    search_queue = false;
+                }
+            }
+        }
+
+        if (queue_cleared == false) {
+            ticks++;
+            std::cout << "Starting tick #" << ticks << std::endl;
+        }
+            
     }
 }
 
@@ -103,14 +212,10 @@ int main() {
 
     for (int i = 0; i < stoi(number_of_calls); i++) {
         Member new_member;
-        std::cin >> new_member.number_of_ticks;
-        std::cout << new_member.number_of_ticks << std::endl;
+        std::cin >> new_member.time_stamp;
         std::cin >> new_member.name;
-        std::cout << new_member.name << std::endl;
         std::cin >> new_member.level;
-        std::cout << new_member.level << std::endl;
         std::cin >> new_member.call_duration;
-        std::cout << new_member.call_duration << std::endl;
 
         member_stack.InsertFront(new_member);
     }
