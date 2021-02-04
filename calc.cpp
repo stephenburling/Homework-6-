76581908dbe6037f4bb5b7e132b69f7f18fd9d53
stephenburling <70167178+stephenburling@users.noreply.github.com>
2021-02-04 17:12:52 -0600

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -3,8 +3,6 @@
 #include <cstring>
 #include "dlist.h"
 
-//hello hello making changes
-
 bool IsValidAction(std::string input_line) {
     if (input_line == "n" || input_line == "d" || input_line == "r" || input_line == "p" || input_line == "c" || input_line == "a") {
         return true;
